bc8203c0ce6f3a95e6452104e72ab3afeb1c8f93
Franny Medico <m.francisca.medico@gmail.com>
2021-11-06 19:34:12 +0100

    added action Types

--- src/App.js
+++ src/App.js
@@ -1,7 +1,87 @@
 import "./App.css";
 
 function App() {
-  return;
+  return (
+    <>
+      <main className="container ">
+        <section className="row justify-content-center">
+          <h1>My Robotos</h1>
+          <div className="col-8 list-container">
+            <ul>
+              <li>Holi</li>
+              <li>Patata</li>
+            </ul>
+          </div>
+          <div className=" col-4 form-container">
+            <form autocomplete="off" onSubmit="" noValidate>
+              <label htmlFor="robotName" className="form-label">
+                Robot Name
+              </label>
+              <input
+                type="text"
+                id="robotName"
+                placeholder="Name"
+                required
+                className="mb-2 form-control"
+              />
+
+              <label htmlFor="robotImage" className="form-label">
+                Robot Image
+              </label>
+              <input
+                type="url"
+                id="robotImage"
+                placeholder="Image"
+                required
+                className="mb-2 form-control"
+              />
+
+              <label htmlFor="robotVelocity" className="form-label">
+                Velocity
+              </label>
+              <input
+                type="number"
+                id="robotVelocity"
+                placeholder="Velocity"
+                required
+                className="mb-2 form-control"
+              />
+
+              <label htmlFor="robotResitance" className="form-label">
+                Resistance
+              </label>
+              <input
+                type="number"
+                id="robotResitance"
+                placeholder="Resistance"
+                required
+                className="mb-2 form-control"
+              />
+              <label htmlFor="robotDate" className="form-label">
+                Creation date
+              </label>
+              <input
+                type="date"
+                id="robotDate"
+                placeholder="dd/mm/yyyy"
+                required
+                className="mb-2 form-control"
+              />
+
+              <button
+                disabled=""
+                onClick=""
+                className="btn btn-primary mt-5"
+                type="submit"
+              >
+                Add Robot
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,9 @@
+const actionTypes = {
+  loadRobots: "LOAD_ROBOTS",
+  loadRobotById: "LOAD_ROBOT_BY_ID",
+  createRobot: "CREATE_ROBOT",
+  deleteRobotById: "DELETE_ROBOT_BY_ID",
+  updateRobot: "UPDATE_ROBOT",
+};
+
+export default actionTypes;
