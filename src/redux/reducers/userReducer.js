e3fe1d09df4b95fc0666278423d80b5b5a96c889
Franny Medico <m.francisca.medico@gmail.com>
2021-11-08 19:09:59 +0100

    added userReducer

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,22 @@
+import actionTypes from "../actions/actionTypes";
+
+const userReducer = (user = { isAuthenticated: false, user: {} }, action) => {
+  let newUser;
+
+  switch (action.type) {
+    case actionTypes.userlogin:
+      newUser = {
+        isAuthenticated: true,
+        user: {},
+      };
+      break;
+    case actionTypes.userLogout:
+      newUser = { isAuthenticated: false, user: {} };
+      break;
+    default:
+      newUser = user;
+  }
+  return newUser;
+};
+
+export default userReducer;
